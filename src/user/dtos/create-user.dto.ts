e1cf3e1f76ba7194d5fea6cc9c0d89365bbaeb06
Jose Martinez <alias_one11@outlook.com>
2021-09-03 01:15:15 -0400

    3. added vaidation to the create user method

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -1,10 +1,26 @@
 /** create-user.dto.ts */
+import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator"
+// ⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰
+
+// class MatchPwd {
+// 	static REGEX = /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/
+// 	static ERROR_MSG = '[ PASSWORD IS TO WEAK PASSWORD ]: Please try again...'
+// }
 // ⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰
 
 export class CreateUserDTO {
+	@IsNotEmpty()//<--validation-pipes
+	@IsString()
+	@MinLength(4)
 	readonly username: string
+	
+	@IsNotEmpty()
+	@IsEmail()
 	readonly email: string
+	
+	@IsNotEmpty()
+	@MinLength(4)
+	// @Matches(MatchPwd.REGEX, { message: MatchPwd.ERROR_MSG })<-- For strong password=PRODUCTION
 	readonly password: string
-
 }
 // ⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰
